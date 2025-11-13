efd243c1089ffe231fdbaa43ef2314d19ade6f41
Priyotush <priyotushpaul40@gmail.com>
2025-11-13 23:33:18 +0600

    update env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdg85wlBNiMdQ9YYXoxBpido5hhxEnn1Q",
-  authDomain: "finease-ae410.firebaseapp.com",
-  projectId: "finease-ae410",
-  storageBucket: "finease-ae410.firebasestorage.app",
-  messagingSenderId: "734722130016",
-  appId: "1:734722130016:web:0a15cf6296e74814e00305",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
